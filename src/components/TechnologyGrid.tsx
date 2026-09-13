2edf075e0b1f9d7270be51a28cf3f2d3014efc39
Jobayer Nayem Afrad <jobayernayemdev01@gmail.com>
2026-09-14 01:35:34 +0600

    Update TechnologyGrid.tsx

--- src/components/TechnologyGrid.tsx
+++ src/components/TechnologyGrid.tsx
@@ -48,16 +48,6 @@ export default function TechnologyGrid({
 
   return (
     <section>
-      <div className="mb-8">
-        <h2 className="text-3xl font-bold text-gray-900">
-          Explore <span className='bg-linear-to-r from-pink-400 to-pink-500 bg-clip-text text-transparent'>Technologies</span>
-        </h2>
-
-        <p className="mt-2 text-gray-500">
-          Discover the technologies you can add to your development stack.
-        </p>
-      </div>
-
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {technologies.map((technology) => (
           <TechnologyCard
